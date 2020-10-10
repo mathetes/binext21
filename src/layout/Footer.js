aa227cd829409e62319269eb0779fc7c74fd82d5
mathetes <mathetes@gmail.com>
2020-10-10 17:19:02 +0700

    General grid

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+ 
+const useStyles = makeStyles(theme => ({
+  footer: {
+    padding: theme.spacing(1),
+    marginTop: 'auto',
+    backgroundColor: 'white',
+  },
+}));
+ 
 export default function Footer () {
-    return (
-        <footer>
-            <h2>FOOTER</h2>
+  const classes = useStyles();
+ 
+  return (
+      <footer className={classes.footer}>
+        <Container maxWidth="xs">
+        <Typography variant="body2" color="textSecondary">
+        
+        </Typography>
+        </Container>
       </footer>
-    )
+    // </div>
+  );
 }
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,14 +1,23 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import Header from './Header';
 import Footer from './Footer';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
 export default function Layout ({children, ...props}) {
+    const classes = useStyles();
     return (
-        <>
+        <div className={classes.root}>
             <Header />
-            <main>{children}</main>
+                <main>{children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
